c53c53dfdd7117faed4a9ee990a9b7af7a62d2ab
Romain Deltour <rdeltour@gmail.com>
2016-02-13 17:00:07 +0100

    Initial version of the slides

---
+++ src/scripts/main.js
@@ -0,0 +1,26 @@
+// Require Node modules in the browser thanks to Browserify: http://browserify.org
+var bespoke = require('bespoke'),
+  classes = require('bespoke-classes'),
+  keys = require('bespoke-keys'),
+  touch = require('bespoke-touch'),
+  bullets = require('bespoke-bullets'),
+  backdrop = require('bespoke-backdrop'),
+  scale = require('bespoke-scale'),
+  hash = require('bespoke-hash');
+
+// Bespoke.js
+bespoke.from('article', [
+  classes(),
+  keys(),
+  touch(),
+  bullets('.bullet'),
+  backdrop(),
+  scale(),
+  hash()
+]);
+
+// Prism syntax highlighting
+// This is actually loaded from "bower_components" thanks to
+// debowerify: https://github.com/eugeneware/debowerify
+require('prism');
+
